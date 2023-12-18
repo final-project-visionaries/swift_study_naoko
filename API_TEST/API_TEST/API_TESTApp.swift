7aa7c2629a36641af03fc837c2235fca6e4c117b
MATSUINAOKO <119477648+MATSUINAOKO@users.noreply.github.com>
2023-12-18 18:28:46 +0900

    add : APIのGETメソッド途中です
    Co-authored-by: Ogata-Kazuyoshi <ogata-kazuyoshi@users.noreply.github.com>
    Co-authored-by: MATSUINAOKO <matsuinaoko@users.noreply.github.com>

---
+++ API_TEST/API_TEST/API_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_TESTApp.swift
+//  API_TEST
+//
+//  Created by user on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct API_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.get("/", (req, res) => {
+  console.log("access is arived!!");
+  const data = [
+    { image_name: "パンダさん", image_data: "16789sdr6yrgfuug2" },
+    { image_name: "ライオンさん", image_data: "16789sdr6yrgfuug3" },
+    { image_name: "カバさん", image_data: "16789sdr6yrgfuug4" },
+  ];
+  res.send(data);
+});
+
+app.listen(8080, () => {
+  console.log("server is running");
+});
